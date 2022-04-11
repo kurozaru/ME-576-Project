632a64b820f8cb5a02afd97479efc585d494deea
kurozaru <pricecs@umich.edu>
2022-04-11 00:32:24 -0400

    FinalSim Updates V1
    completed all 3 battery manufacturer simulations

--- ME_576_Project_Code.m
+++ ME_576_Project_Code.m
@@ -4,43 +4,76 @@ clc
 %% Open and Sim
 % Baseline @ 25C
 %open power_battery_aging_sim.slx
-sim power_battery_aging_sim.slx
+%sim power_battery_aging_sim.slx
+
 % Arizona @ 29.67C
 %open power_battery_aging_sim_Arizona.slx
-sim power_battery_aging_sim_Arizona.slx
+%sim power_battery_aging_sim_Arizona.slx
+
 % Florida @ 22.3C
 %open power_battery_aging_sim_Florida.slx
-sim power_battery_aging_sim_Florida.slx
+%sim power_battery_aging_sim_Florida.slx
 
 % Michigan @ 15C
 %open power_battery_aging_sim_Michigan.slx
-sim power_battery_aging_sim_Michigan.slx
+%sim power_battery_aging_sim_Michigan.slx
 
 % Alaska @ 7C
 %open power_battery_aging_sim_Alaska.slx
-sim power_battery_aging_sim_Alaska.slx
+%sim power_battery_aging_sim_Alaska.slx
 
 % Tesla - baseline
 %open power_battery_aging_sim_TeslaBaseline.slx
-sim power_battery_aging_sim_TeslaBaseline2.slx
+sim power_battery_aging_sim_TeslaBaseline.slx
 
 % Tesla - Arizona
 %open power_battery_aging_sim_TeslaArizona.slx
-sim power_battery_aging_sim_TeslaArizona2.slx
+sim power_battery_aging_sim_TeslaArizona.slx
 
 % Tesla - Alaska
 %open power_battery_aging_sim_TeslaAlaska.slx
-sim power_battery_aging_sim_TeslaAlaska2.slx
+sim power_battery_aging_sim_TeslaAlaska.slx
 
 % Tesla - Michigan
 %open power_battery_aging_sim_TeslaMichigan.slx
-sim power_battery_aging_sim_TeslaMichigan2.slx
+sim power_battery_aging_sim_TeslaMichigan.slx
 
 % Tesla - Florida
 %open power_battery_aging_sim_TeslaFlorida.slx
-sim power_battery_aging_sim_TeslaFlorida2.slx
+sim power_battery_aging_sim_TeslaFlorida.slx
+
+% Samsung - baseline
+sim power_battery_aging_sim_SamsungBaseline.slx
+
+% Samsung - Arizona
+sim power_battery_aging_sim_SamsungArizona.slx
+
+% Samsung - Alaska
+sim power_battery_aging_sim_SamsungAlaska.slx
+
+% Samsung - Michigan
+sim power_battery_aging_sim_SamsungMichigan.slx
+
+% Samsung - Florida
+sim power_battery_aging_sim_SamsungFlorida.slx
+
+% SK - baseline
+sim power_battery_aging_sim_SKBaseline.slx
+
+% SK - Arizona
+sim power_battery_aging_sim_SKArizona.slx
+
+% SK - Alaska
+sim power_battery_aging_sim_SKAlaska.slx
+
+% SK - Michigan
+sim power_battery_aging_sim_SKMichigan.slx
+
+% SK - Florida
+sim power_battery_aging_sim_SKFlorida.slx
 
 %% Initiate Variables from Sim - initial battery 12.8v, 40Ah, Li-ion
+%{
 % Baseline
 BaseT = timeseries2timetable(Baseline.Cell_Temperature__oC_);
 BaseAge = timeseries2timetable(Baseline.Age__Equivalent_Full_Cycles_);
@@ -75,8 +108,8 @@ AAge = timeseries2timetable(Alaska.Age__Equivalent_Full_Cycles_);
 ACap = timeseries2timetable(Alaska.Maximum_Capacity___Ah_);
 ATime = AT.Time;
 ATT = AAge.Time;
-
-%% Initialize Variables for Telsa Battery Module Sim
+%}
+%% Initialize Variables for Telsa Battery Cell Sim
 % Baseline
 TeslaBaseT = timeseries2timetable(TeslaBaseline.Cell_Temperature__oC_);
 TeslaAge = timeseries2timetable(TeslaBaseline.Age__Equivalent_Full_Cycles_);
@@ -112,8 +145,80 @@ TeslaFloCap = timeseries2timetable(TeslaFlorida.Maximum_Capacity___Ah_);
 TeslaFloTime = TeslaFloT.Time;
 TeslaFloTT = TeslaFloAge.Time;
 
-%% Plot initial battery 12.8v, 40Ah, Li-ion
+%% Initialize Variables for Samsung Cells
+% Baseline
+SamBaseT = timeseries2timetable(SamsungBaseline.Cell_Temperature__oC_);
+SamAge = timeseries2timetable(SamsungBaseline.Age__Equivalent_Full_Cycles_);
+SamCap = timeseries2timetable(SamsungBaseline.Maximum_Capacity___Ah_);
+SamTTime = SamBaseT.Time;
+SamTT = SamAge.Time;
 
+% Arizona
+SamAZT = timeseries2timetable(SamsungArizona.Cell_Temperature__oC_);
+SamAZAge = timeseries2timetable(SamsungArizona.Age__Equivalent_Full_Cycles_);
+SamAZCap = timeseries2timetable(SamsungArizona.Maximum_Capacity___Ah_);
+SamAZTime = SamAZT.Time;
+SamAZTT = SamAZAge.Time;
+
+% Alaska
+SamAT = timeseries2timetable(SamsungAlaska.Cell_Temperature__oC_);
+SamAAge = timeseries2timetable(SamsungAlaska.Age__Equivalent_Full_Cycles_);
+SamACap = timeseries2timetable(SamsungAlaska.Maximum_Capacity___Ah_);
+SamATime = SamAT.Time;
+SamATT = SamAAge.Time;
+
+% Michigan
+SamMiT = timeseries2timetable(SamsungMichigan.Cell_Temperature__oC_);
+SamMiAge = timeseries2timetable(SamsungMichigan.Age__Equivalent_Full_Cycles_);
+SamMiCap = timeseries2timetable(SamsungMichigan.Maximum_Capacity___Ah_);
+SamMiTime = SamMiT.Time;
+SamMiTT = SamMiAge.Time;
+
+% Florida
+SamFloT = timeseries2timetable(SamsungFlorida.Cell_Temperature__oC_);
+SamFloAge = timeseries2timetable(SamsungFlorida.Age__Equivalent_Full_Cycles_);
+SamFloCap = timeseries2timetable(SamsungFlorida.Maximum_Capacity___Ah_);
+SamFloTime = SamFloT.Time;
+SamFloTT = SamFloAge.Time;
+
+%% Initialize Variables for SK Cells
+% Baseline
+SKBaseT = timeseries2timetable(SKBaseline.Cell_Temperature__oC_);
+SKAge = timeseries2timetable(SKBaseline.Age__Equivalent_Full_Cycles_);
+SKCap = timeseries2timetable(SKBaseline.Maximum_Capacity___Ah_);
+SKTTime = SKBaseT.Time;
+SKTT = SKAge.Time;
+
+% Arizona
+SKAZT = timeseries2timetable(SKArizona.Cell_Temperature__oC_);
+SKAZAge = timeseries2timetable(SKArizona.Age__Equivalent_Full_Cycles_);
+SKAZCap = timeseries2timetable(SKArizona.Maximum_Capacity___Ah_);
+SKAZTime = SKAZT.Time;
+SKAZTT = SKAZAge.Time;
+
+% Alaska
+SKAT = timeseries2timetable(SKAlaska.Cell_Temperature__oC_);
+SKAAge = timeseries2timetable(SKAlaska.Age__Equivalent_Full_Cycles_);
+SKACap = timeseries2timetable(SKAlaska.Maximum_Capacity___Ah_);
+SKATime = SKAT.Time;
+SKATT = SKAAge.Time;
+
+% Michigan
+SKMiT = timeseries2timetable(SKMichigan.Cell_Temperature__oC_);
+SKMiAge = timeseries2timetable(SKMichigan.Age__Equivalent_Full_Cycles_);
+SKMiCap = timeseries2timetable(SKMichigan.Maximum_Capacity___Ah_);
+SKMiTime = SKMiT.Time;
+SKMiTT = SKMiAge.Time;
+
+% Florida
+SKFloT = timeseries2timetable(SKFlorida.Cell_Temperature__oC_);
+SKFloAge = timeseries2timetable(SKFlorida.Age__Equivalent_Full_Cycles_);
+SKFloCap = timeseries2timetable(SKFlorida.Maximum_Capacity___Ah_);
+SKFloTime = SKFloT.Time;
+SKFloTT = SKFloAge.Time;
+
+%% Plot initial battery 12.8v, 40Ah, Li-ion
+%{
 figure('Name','Cell Temp.(C)');
 plot(BTime,BaseT.Cell_Temperature__oC_(:,1),'k',AZTime,AZT.Cell_Temperature__oC_(:,1),'r',...
     FloTime,FloT.Cell_Temperature__oC_(:,1),'b',MiTime,MiT.Cell_Temperature__oC_(:,1),'g',...
@@ -140,8 +245,37 @@ xlabel('Time (sec)');
 ylabel('Age');
 legend('Base','Arizona','Florida','Michigan','Alaska');
 title('Battery Age');
+%}
+%% Plot Samsung 3.6v, 94Ah, Li-ion
 
-%% Plot Tesla Module 22.8v, 232Ah, Li-ion
+figure('Name','Samsung Cell Temp.(C)');
+plot(SamTTime,SamBaseT.Cell_Temperature__oC_(:,1),'k',SamAZTime,SamAZT.Cell_Temperature__oC_(:,1),'r',...
+    SamFloTime,SamFloT.Cell_Temperature__oC_(:,1),'b',SamMiTime,SamMiT.Cell_Temperature__oC_(:,1),'g',...
+    SamATime,SamAT.Cell_Temperature__oC_(:,1),'c');
+xlabel('Time (sec)');
+ylabel('Temp (C)');
+legend('Base','Arizona','Florida','Michigan','Alaska',Location='northwest');
+title('Samsung Cell Temp(C)');
+
+figure('Name','Samsung Capacity(Ah)');
+plot(SamCap.Time,SamCap.Maximum_Capacity___Ah_(:,1),'k',SamAZCap.Time,SamAZCap.Maximum_Capacity___Ah_(:,1),'r',...
+    SamFloCap.Time,SamFloCap.Maximum_Capacity___Ah_(:,1),'b',SamMiCap.Time,SamMiCap.Maximum_Capacity___Ah_(:,1),'g',...
+    SamACap.Time,SamACap.Maximum_Capacity___Ah_(:,1),'c');
+xlabel('Time (sec)');
+ylabel('Capacity (Ah)');
+legend('Base','Arizona','Florida','Michigan','Alaska');
+title('Samsung Battery Capacity (Ah)');
+
+figure('Name','Samsung Age');
+plot(SamTT,SamAge.Age__Equivalent_Full_Cycles_(:,1),'k',SamAZAge.Time,SamAZAge.Age__Equivalent_Full_Cycles_(:,1),'r',...
+    SamFloTT,SamFloAge.Age__Equivalent_Full_Cycles_(:,1),'b',SamMiTT,SamMiAge.Age__Equivalent_Full_Cycles_(:,1),'g',...
+    SamATT,SamAAge.Age__Equivalent_Full_Cycles_(:,1),'c');
+xlabel('Time (sec)');
+ylabel('Age');
+legend('Base','Arizona','Florida','Michigan','Alaska',Location='northwest');
+title('Samsung Battery Age');
+
+%% Plot Tesla Module 3.8v, 38Ah, Li-ion
 
 figure('Name','Tesla Cell Temp.(C)');
 plot(TTime,TeslaBaseT.Cell_Temperature__oC_(:,1),'k',TeslaAZTime,TeslaAZT.Cell_Temperature__oC_(:,1),'r',...
@@ -149,7 +283,8 @@ plot(TTime,TeslaBaseT.Cell_Temperature__oC_(:,1),'k',TeslaAZTime,TeslaAZT.Cell_T
     TeslaATime,TeslaAT.Cell_Temperature__oC_(:,1),'c');
 xlabel('Time (sec)');
 ylabel('Temp (C)');
-legend('Base','Arizona','Florida','Michigan','Alaska');
+legend('Base','Arizona','Florida','Michigan','Alaska',Location='northwest');
+legend(loc='upper left')
 title('Tesla Cell Temp(C)');
 
 figure('Name','Tesla Capacity(Ah)');
@@ -167,5 +302,34 @@ plot(TT,TeslaAge.Age__Equivalent_Full_Cycles_(:,1),'k',TeslaAZAge.Time,TeslaAZAg
     TeslaATT,TeslaAAge.Age__Equivalent_Full_Cycles_(:,1),'c');
 xlabel('Time (sec)');
 ylabel('Age');
-legend('Base','Arizona','Florida','Michigan','Alaska');
+legend('Base','Arizona','Florida','Michigan','Alaska',Location='northwest');
 title('Tesla Battery Age');
+
+%% Plot SK 3.7v, 45Ah, Li-ion
+
+figure('Name','SK Cell Temp.(C)');
+plot(SKTTime,SKBaseT.Cell_Temperature__oC_(:,1),'k',SKAZTime,SKAZT.Cell_Temperature__oC_(:,1),'r',...
+    SKFloTime,SKFloT.Cell_Temperature__oC_(:,1),'b',SKMiTime,SKMiT.Cell_Temperature__oC_(:,1),'g',...
+    SKATime,SKAT.Cell_Temperature__oC_(:,1),'c');
+xlabel('Time (sec)');
+ylabel('Temp (C)');
+legend('Base','Arizona','Florida','Michigan','Alaska',Location='northwest');
+title('SK Cell Temp(C)');
+
+figure('Name','SK Capacity(Ah)');
+plot(SKCap.Time,SKCap.Maximum_Capacity___Ah_(:,1),'k',SKAZCap.Time,SKAZCap.Maximum_Capacity___Ah_(:,1),'r',...
+    SKFloCap.Time,SKFloCap.Maximum_Capacity___Ah_(:,1),'b',SKMiCap.Time,SKMiCap.Maximum_Capacity___Ah_(:,1),'g',...
+    SKACap.Time,SKACap.Maximum_Capacity___Ah_(:,1),'c');
+xlabel('Time (sec)');
+ylabel('Capacity (Ah)');
+legend('Base','Arizona','Florida','Michigan','Alaska');
+title('SK Battery Capacity (Ah)');
+
+figure('Name','SK Age');
+plot(SKTT,SKAge.Age__Equivalent_Full_Cycles_(:,1),'k',SKAZAge.Time,SKAZAge.Age__Equivalent_Full_Cycles_(:,1),'r',...
+    SKFloTT,SKFloAge.Age__Equivalent_Full_Cycles_(:,1),'b',SKMiTT,SKMiAge.Age__Equivalent_Full_Cycles_(:,1),'g',...
+    SKATT,SKAAge.Age__Equivalent_Full_Cycles_(:,1),'c');
+xlabel('Time (sec)');
+ylabel('Age');
+legend('Base','Arizona','Florida','Michigan','Alaska',Location='northwest');
+title('SK Battery Age');
